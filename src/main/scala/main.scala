89f988cc88c0c930bb34f67146c7984f1121d672
gnadigpeter <xlurzx@gmail.com>
2024-09-30 09:19:02 +0200

    tuples

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,12 +2,14 @@
 @main
 def main(): Unit = {
   println("Hello world!")
-//  FunWithScala.BasicFun
-//  FunWithScala.UnifiedTypes
-//  FunWithScala.Classes.PlayGround
-//  FunWithScala.DefaultParameterValues.PlayGround
-//  FunWithScala.NamedArguments.PlayGround
-//  FunWithScala.Traits.PlayGround
+  //  FunWithScala.BasicFun
+  //  FunWithScala.UnifiedTypes
+  //  FunWithScala.Classes.PlayGround
+  //  FunWithScala.DefaultParameterValues.PlayGround
+  //  FunWithScala.NamedArguments.PlayGround
+  //  FunWithScala.Traits.PlayGround
+  //  FunWithScala.Tuples.PlayGround
+
 
 
 }
