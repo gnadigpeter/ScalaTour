7a20b646e3ea4864cdc6c263f954d0af4e609c3c
gnadigpeter <xlurzx@gmail.com>
2024-10-01 09:24:59 +0200

    fix

--- src/main/scala/FunWithScala/ForComprehensions/PlayGround.scala
+++ src/main/scala/FunWithScala/ForComprehensions/PlayGround.scala
@@ -14,9 +14,26 @@ object PlayGround {
       yield user.name // i.e. add this to a list
 
   twentySomethings.foreach(println) // prints Travis Dennis
-  
-  
-  
+
+
+  def foo(n: Int, v: Int) =
+    for (i <- 0 until n;
+         j <- 0 until n if i + j == v)
+    yield (i, j)
+
+  foo(10, 10).foreach {
+    case (i, j) =>
+      println(s"($i, $j) ") // prints (1, 9) (2, 8) (3, 7) (4, 6) (5, 5) (6, 4) (7, 3) (8, 2) (9, 1)
+  }
+  val test = foo(10, 10)
+  test.foreach { (i) => println(i) }
+
+  def foo2(n: Int, v: Int) =
+    for (i <- 0 until n;
+         j <- 0 until n if i + j == v)
+      println(s"($i, $j)")
+
+  foo2(10, 10)
   
   
   
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -18,24 +18,7 @@ def main(): Unit = {
   //  FunWithScala.SingeltonObjects.PlayGround
   //  FunWithScala.RegularExpressionPatterns.PlayGround
   //  FunWithScala.ExtractorObjects.PlayGround
-//  FunWithScala.ForComprehensions.PlayGround
+  //  FunWithScala.ForComprehensions.PlayGround
 
-  def foo(n: Int, v: Int) =
-    for (i <- 0 until n;
-         j <- 0 until n if i + j == v)
-    yield (i, j)
 
-  foo(10, 10).foreach {
-    case (i, j) =>
-      println(s"($i, $j) ")  // prints (1, 9) (2, 8) (3, 7) (4, 6) (5, 5) (6, 4) (7, 3) (8, 2) (9, 1)
-  }
-  val test = foo(10,10)
-  test.foreach{(i) => println(i)}
-
-  def foo2(n: Int, v: Int) =
-    for (i <- 0 until n;
-         j <- 0 until n if i + j == v)
-      println(s"($i, $j)")
-
-  foo2(10, 10)
 }
